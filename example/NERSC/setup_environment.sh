e900aff1f4f10f493eeb3094a19ec99bfa03c702
Karen Y. Ng <karen.y.ng@intel.com>
2017-10-06 16:47:39 -0700

    Update the name of the Conda hugelbfs package

--- example/NERSC/setup_environment.sh
+++ example/NERSC/setup_environment.sh
@@ -4,7 +4,7 @@ if [[ ! -d $HOME/.conda ]]; then
   mkdir $HOME/.conda
 fi
 
-conda create -n wrapped_ibench -c intel -c intel/label/test -y wrappython python=3.6 scipy
+conda create -n wrapped_ibench -c intel -c intel/label/hugetlbfs -y python=3.6 hugetlbfs scipy
 source $HOME/.conda/envs/ibench/bin/activate wrapped_ibench
 cd ../../
 python setup.py install
