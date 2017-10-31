d7253790cdf2e4f997ad23668156d76522a9c231
Karen Ng <karen.yyng@gmail.com>
2017-10-30 17:38:02 -0700

    Fix path typo per @nalinik 's suggestion

--- example/NERSC/setup_environment.sh
+++ example/NERSC/setup_environment.sh
@@ -5,6 +5,6 @@ if [[ ! -d $HOME/.conda ]]; then
 fi
 
 conda create -n wrapped_ibench -c intel -y python=3.6 hugetlbfs scipy
-source $HOME/.conda/envs/ibench/bin/activate wrapped_ibench
+source $HOME/.conda/envs/wrapped_ibench/bin/activate wrapped_ibench
 cd ../../
 python setup.py install
