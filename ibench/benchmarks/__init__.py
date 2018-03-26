aa8d7f325b0f9d6d13f042f0775212682baaf8f5
triskadecaepyon <dcltechnology@gmail.com>
2018-03-26 13:59:51 -0500

    Adding RNG, and creating an update 2018 benchmark

--- ibench/benchmarks/__init__.py
+++ ibench/benchmarks/__init__.py
@@ -7,6 +7,7 @@ from . import lu
 from . import qr
 from . import svd
 from . import blacksch
+from . import rng
 
 benchmarks = {
     'cholesky': cholesky.Cholesky,
@@ -17,11 +18,12 @@ benchmarks = {
     'lu': lu.Lu,
     'qr': qr.Qr,
     'svd': svd.Svd,
-    'blacksch':blacksch.Blacksch
+    'blacksch':blacksch.Blacksch,
+    'rng':rng.Rng
 }
 
 benchmark_groups = {
     'linalg': ['cholesky', 'det', 'dot', 'inv', 'lu', 'qr', 'svd'],
     'all': list(benchmarks.keys()),
-    'bench2018': ['fft','lu','dot','blacksch']
+    'bench2018': ['fft', 'lu', 'dot', 'cholesky', 'qr', 'blacksch', 'rng']
 }
