53f4f20a70182d85c5cb4f539c86341eb3582722
triskadecaepyon <dcltechnology@gmail.com>
2018-03-27 15:53:25 -0500

    adding in all the scikit-learn benchmarks

---
+++ ibench/benchmarks/lregressionfit.py
@@ -0,0 +1,33 @@
+import sklearn
+import multiprocessing
+from numpy.random import rand
+from sklearn import linear_model
+
+from .bench import Bench
+
+if sklearn.__version__ == '0.18.2':
+    sklearn.utils.validation._assert_all_finite = lambda X: None 
+
+class Lregressionfit(Bench):
+    """
+    Benchmark for Linear Regression Prediction from Scikit-learn
+    Attempts to utilize parallelism for larger datasets
+    """
+    sizes = {'large': 10000, 'small': 5000, 'tiny': 1000, 'test': 10}
+
+    def _ops(self, n):
+        return 2E-9 * n*n*n
+
+    def _make_args(self, n):
+        p = int(n/10)
+        self._X = rand(p,n)
+        self._y = rand(p,n)
+        if n < 8000:
+            self._regr = linear_model.LinearRegression()
+        else:
+            self._regr = linear_model.LinearRegression(n_jobs=-1)
+
+        self._regr.fit(self._X,self._y)
+
+    def _compute(self):
+        self._trained_model =  self._regr.predict(self._X)
---
+++ ibench/benchmarks/ridge.py
@@ -0,0 +1,29 @@
+import numpy as np
+import sklearn
+import multiprocessing
+from numpy.random import rand
+from sklearn import linear_model
+
+from .bench import Bench
+
+if sklearn.__version__ == '0.18.2':
+    sklearn.utils.validation._assert_all_finite = lambda X: None 
+
+class Ridge(Bench):
+    """
+    Benchmark for Ridge Regression Training from Scikit-learn
+    Attempts to utilize parallelism for larger datasets
+    """
+    sizes = {'large': 10000, 'small': 5000, 'tiny': 1000, 'test': 10}
+
+    def _ops(self, n):
+        return 2E-9 * n*n*n
+
+    def _make_args(self, n):
+        p = int(n/10)
+        self._X = rand(p,n)
+        self._y = rand(p,n)
+        self._regr = linear_model.Ridge() 
+
+    def _compute(self):
+        self._trained_model =  self._regr.fit(self._X,self._y)
