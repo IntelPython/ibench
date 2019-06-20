93c27df24d6e004b973ac85e458654a160190835
Sameer Bibikar <sameer.bibikar@intel.com>
2019-06-20 14:32:14 -0500

    Add CSV output, fix env info collection (#8)
    * Add CSV output, fix env info collection
    
    * Bump copyright year ranges
    
    * Run cmds/run.py through flake8

--- ibench/cmds/run.py
+++ ibench/cmds/run.py
@@ -1,53 +1,63 @@
-# Copyright (C) 2016-2018 Intel Corporation
+# Copyright (C) 2016-2019 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
 
 from __future__ import print_function
 
-import argparse
+import csv
 import datetime
 import json
 import os
 import platform
 import subprocess
 import sys
-import time
 
 from ..benchmarks import benchmarks
 from ..benchmarks import benchmark_groups
 from .cmd import Cmd
 
+
 def capture_multiline_output(command):
     try:
-        return str(subprocess.check_output(command,shell=True,stderr=subprocess.STDOUT)).split('\\n')
-    except:
+        out = subprocess.check_output(command, shell=True,
+                                      stderr=subprocess.STDOUT)
+        return out.decode().split('\n')
+    except subprocess.CalledProcessError:
         return ''
 
 
 def add_parser(subparsers):
     parser = subparsers.add_parser('run')
-    parser.add_argument('-b','--benchmarks', 
-                        choices=list(benchmarks.keys()) + list(benchmark_groups.keys()),
-                        nargs='+', 
+    parser.add_argument('-b', '--benchmarks',
+                        choices=list(benchmarks.keys())
+                        + list(benchmark_groups.keys()),
+                        nargs='+',
                         help='Benchmarks to run')
-    parser.add_argument('--file', 
+    parser.add_argument('--file',
                         help='Write results to <file> instead of stdout')
-    parser.add_argument('--name', 
-                        default='noname', 
-                        help='Descriptive name of run to include in results file')
-    parser.add_argument('--size', 
-                        choices=['large','small', 'tiny', 'test'],
+    parser.add_argument('--name', default='noname',
+                        help='Descriptive name of run to include in results')
+    parser.add_argument('--size',
+                        choices=['large', 'small', 'tiny', 'test'],
                         default='test',
-                        help='Size of workload to run. In general, use large for multicore, small for single thread, and test for debugging')
-    parser.add_argument('-q', 
-                        '--quiet', 
-                        default=False, 
-                        action='store_true', 
-                        help="Logging")
+                        help='Size of workload to run. In general, use large '
+                             'for multicore, small for single thread, and '
+                             'test for debugging')
+    parser.add_argument('-q', '--quiet', default=False, action='store_true',
+                        help='Suppress logging outputs')
     parser.add_argument('--runs', default=3, type=int, help='Number of runs')
     parser.add_argument('--gflops', default=False,
                         help='Turn on approximate gflops for test saturation')
+    parser.add_argument('--no-get-env-info', dest='get_env_info',
+                        action='store_false', default=True,
+                        help='Skip getting environment information')
+    parser.add_argument('-f', '--format', choices=['json', 'csv'],
+                        default='json', help='Output format')
+    parser.add_argument('--env-info-prefix', default='@',
+                        help='Character to put in front of each line of env '
+                             'info when using csv output')
     parser.set_defaults(func=Run)
+
 
 class Run(Cmd):
     results = {}
@@ -67,7 +77,11 @@ class Run(Cmd):
     def _parse_bench(self):
         default_bench = ['dot']
         self._bmarks = []
-        bstack = self.args.benchmarks if self.args.benchmarks else default_bench
+
+        bstack = self.args.benchmarks
+        if bstack is None:
+            bstack = default_bench
+
         for bs in bstack:
             # expand groups
             if bs in benchmark_groups:
@@ -82,7 +96,6 @@ class Run(Cmd):
                       file=sys.stderr)
                 sys.exit(1)
 
-
     def _set_from_environ(self, key):
         self.results[key] = os.environ[key] if key in os.environ else 'not set'
 
@@ -94,16 +107,49 @@ class Run(Cmd):
         self._set_from_environ('KMP_AFFINITY')
         self._set_from_environ('OMP_NUM_THREADS')
         self._set_from_environ('MKL_NUM_THREADS')
-        results['host'] = platform.node()
-        results['lscpu'] = capture_multiline_output('lscpu')
-        results['numactl'] = capture_multiline_output('numactl --show')
-        results['pip list'] = capture_multiline_output('/usr/bin/pip list')
-        results['conda'] = capture_multiline_output('conda list')
+
+        if self.args.get_env_info:
+            results['host'] = platform.node()
+            results['lscpu'] = capture_multiline_output('lscpu')
+            results['numactl'] = capture_multiline_output('numactl --show')
+            results['pip list'] = capture_multiline_output('/usr/bin/pip list')
+            results['conda'] = capture_multiline_output('conda list')
+
         results['runs'] = []
 
     def _write_output(self):
         filename = self.args.file
         fh = open(filename, 'w') if filename else sys.stdout
-        json.dump(self.results,fh,indent=2)
+
+        if self.args.format == 'json':
+            json.dump(self.results, fh, indent=2)
+        elif self.args.format == 'csv':
+            for env_key in self.results.keys() - {'name', 'date', 'runs'}:
+
+                # Output environment information in an easily cleanable way
+                # We will prefix each line with something like "@ "
+                env_prefix = self.args.env_info_prefix
+
+                env_info = self.results[env_key]
+                if type(env_info) is str:
+                    fh.write('{0}{0} {1}={2}\n'.format(env_prefix, env_key,
+                                                       env_info))
+                else:
+                    fh.write('{0}{0} {1}=\n'.format(env_prefix, env_key))
+                    for line in [env_key + ":"] + env_info:
+                        fh.write('{0} {1}\n'.format(env_prefix, line))
+
+            # Now, for the actual CSV output...
+            writer = csv.writer(fh, delimiter=',')
+            writer.writerow(['Prefix', 'Function', 'Size', 'Time'])
+
+            all_runs_output = [self.results['name']]
+
+            for bench in self.results['runs']:
+                bench_output = all_runs_output + [bench['name'], bench['N']]
+                for time in bench['times']:
+                    time_output = bench_output + [time]
+                    writer.writerow(time_output)
+
         if filename:
             fh.close()
