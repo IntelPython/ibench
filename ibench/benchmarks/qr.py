2d523262dd3b36a5774eb32576339e16800ce20e
Sameer Bibikar <sameer.bibikar@intel.com>
2019-07-10 10:02:34 -0500

    Use asfortranarray for qr, svd, and
    use full_matrices=True for scipy.linalg.svd. This puts jobz='A' in the
    call to LAPACK dgesdd, which is actually faster than jobz='S' with MKL.

--- ibench/benchmarks/qr.py
+++ ibench/benchmarks/qr.py
@@ -7,14 +7,16 @@ import scipy
 
 from .bench import Bench
 
+
 class Qr(Bench):
-    sizes = {'large': 10000, 'small': 5000, 'tiny': 1000,'test': 2}
+    sizes = {'large': 10000, 'small': 5000, 'tiny': 1000, 'test': 2}
 
     def _ops(self, n):
         return (4./3.)*n*n*n*1e-9
 
     def _make_args(self, n):
-        self._A = np.asarray(np.random.rand(n,n), dtype=self._dtype)
+        self._A = np.asfortranarray(np.random.rand(n, n), dtype=self._dtype)
 
     def _compute(self):
-        scipy.linalg.qr(self._A, overwrite_a=True, check_finite=False, mode='raw')
+        scipy.linalg.qr(self._A, overwrite_a=True, check_finite=False,
+                        mode='raw')
--- ibench/benchmarks/svd.py
+++ ibench/benchmarks/svd.py
@@ -7,6 +7,7 @@ import scipy.linalg
 
 from .bench import Bench
 
+
 class Svd(Bench):
     sizes = {'large': 10000, 'small': 5000, 'tiny': 1000, 'test': 2}
 
@@ -14,7 +15,8 @@ class Svd(Bench):
         return (4./3.)*n*n*n*1e-9
 
     def _make_args(self, n):
-        self._A = np.asarray(np.random.rand(n,n), dtype=self._dtype)
+        self._A = np.asfortranarray(np.random.rand(n, n), dtype=self._dtype)
 
     def _compute(self):
-        scipy.linalg.svd(self._A, overwrite_a=True, check_finite=False, full_matrices=False)
+        scipy.linalg.svd(self._A, overwrite_a=True, check_finite=False,
+                         full_matrices=True)
