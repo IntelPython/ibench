a6ac01300bf73233457e0380c8fefe888ac5c047
Sameer Bibikar <sameer.bibikar@intel.com>
2019-07-18 17:23:20 -0500

    Use F-contiguous arrays for QR, SVD (#10)
    * Use asfortranarray for qr, svd, and
    
    use full_matrices=True for scipy.linalg.svd. This puts jobz='A' in the
    call to LAPACK dgesdd, which is actually faster than jobz='S' with MKL.
    
    * svd: revert to full_matrices=False
    
    * use overwrite_a=False for scipy.linalg.svd

--- ibench/benchmarks/svd.py
+++ ibench/benchmarks/svd.py
@@ -7,6 +7,7 @@ import scipy.linalg
 
 from .bench import Bench
 
+
 class Svd(Bench):
     sizes = {'large': 10000, 'small': 5000, 'tiny': 1000, 'test': 2}
 
@@ -14,7 +15,9 @@ class Svd(Bench):
         return (4./3.)*n*n*n*1e-9
 
     def _make_args(self, n):
-        self._A = np.asarray(np.random.rand(n,n), dtype=self._dtype)
+        self._A = np.asfortranarray(np.random.rand(n, n), dtype=self._dtype)
 
     def _compute(self):
-        scipy.linalg.svd(self._A, overwrite_a=True, check_finite=False, full_matrices=False)
+        # We specify overwrite_a=False here because once the input array
+        # is overwritten, dgesdd might decide to terminate early
+        scipy.linalg.svd(self._A, overwrite_a=False, check_finite=False)
