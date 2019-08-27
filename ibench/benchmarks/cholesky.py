ba61c7e5f6061a24ef7d19392fef9caba04bde84
Sameer Bibikar <sameer.bibikar@intel.com>
2019-08-27 15:16:32 -0500

    Perform copy in cholesky benchmark (#11)
    * Perform copy in cholesky benchmark
    
    * Actually perform matrix multiplication in cholesky make_args

--- ibench/benchmarks/cholesky.py
+++ ibench/benchmarks/cholesky.py
@@ -7,6 +7,7 @@ import scipy.linalg
 
 from .bench import Bench
 
+
 class Cholesky(Bench):
     sizes = {'large': 40000, 'small': 10000, 'tiny': 2000, 'test': 2}
 
@@ -14,8 +15,9 @@ class Cholesky(Bench):
         return n*n*n/3.0*1e-9
 
     def _make_args(self, n):
-        self._A = np.asarray(np.random.rand(n,n), dtype=self._dtype)
-        self._A = np.asfortranarray(self._A*self._A.transpose() + n*np.eye(n))
+        self._A = np.asarray(np.random.rand(n, n), dtype=self._dtype)
+        self._A = np.asfortranarray(self._A.T @ self._A + n * np.eye(n))
 
     def _compute(self):
-        scipy.linalg.cholesky(self._A, lower=False, overwrite_a=True, check_finite=False)
+        scipy.linalg.cholesky(self._A, lower=False, overwrite_a=False,
+                              check_finite=False)
